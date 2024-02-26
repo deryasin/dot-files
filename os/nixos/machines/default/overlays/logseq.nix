3ccb54121700a46c8962da678d2289dd916a1f3b
Yasin <yasintikdemir2002@gmail.com>
2024-02-26 20:31:01 +0100

    mobile - rip

--- os/nixos/machines/default/overlays/logseq.nix
+++ os/nixos/machines/default/overlays/logseq.nix
@@ -4,17 +4,13 @@
 nixpkgs.overlays = [
   (
     final: prev: {
-      logseq = prev.logseq.overrideAttrs (oldAttrs: {
-        postFixup = ''
-          makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
-            --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
-            --add-flags $out/share/${oldAttrs.pname}/resources/app \
-            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-            --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
-        '';
-      });
-    }
+    logseq = prev.logseq.overrideAttrs (oldAttrs: {
+      postFixup = ''
+        makeWrapper ${prev.electron_27}/bin/electron $out/bin/${oldAttrs.pname} \
+          --add-flags $out/share/${oldAttrs.pname}/resources/app \
+          --add-flags "--use-gl=desktop" \
+          --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [prev.stdenv.cc.cc.lib]}"
+      '';
+    });
   )
-];
-
-}
+]
