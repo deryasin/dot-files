b583c33da36f9449006758bebf2f812193661cef
yasin.work <yasintikdemir2002@gmail.com>
2024-05-04 14:22:11 +0200

    74 current  2024-05-04 14:22:02  24.05.19700101.dirty     6.6.22                          *

--- nixos/.config/nixos/default/modules/environment/default/pkgs.nix
+++ nixos/.config/nixos/default/modules/environment/default/pkgs.nix
@@ -115,6 +115,7 @@
     fuse3
     unstable.nh
     pika-backup
+    gimp
   ];
   environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
